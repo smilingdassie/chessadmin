1bbfe6ad901d55146ed0873b4226949507a30e32
smilingdassie <smilingdassie@gmail.com>
2022-06-12 20:09:05 +0200

    Web application basic

---
+++ ChessAdminWebMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ChessAdminWebMVC.Startup))]
+namespace ChessAdminWebMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
