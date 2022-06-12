c2c3e068344a9c4e0132d965d5c6e17b59f2e06d
smilingdassie <smilingdassie@gmail.com>
2022-06-12 20:20:40 +0200

    Models for Game, Member CRUD screens for Members, Games

---
+++ ChessAdminWebMVC/Controllers/GamesController.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ChessAdminWebMVC;
+
+namespace ChessAdminWebMVC.Controllers
+{
+    public class GamesController : Controller
+    {
+        private ChessAdminDbEntities1 db = new ChessAdminDbEntities1();
+
+        // GET: Games
+        public ActionResult Index()
+        {
+            var games = db.Games.Include(g => g.Member).Include(g => g.Member1).Include(g => g.Member2);
+            return View(games.ToList());
+        }
+
+        // GET: Games/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Game game = db.Games.Find(id);
+            if (game == null)
+            {
+                return HttpNotFound();
+            }
+            return View(game);
+        }
+
+        // GET: Games/Create
+        public ActionResult Create()
+        {
+            ViewBag.PlayerOneID = new SelectList(db.Members, "ID", "Name");
+            ViewBag.PlayerTwoID = new SelectList(db.Members, "ID", "Name");
+            ViewBag.WinnerID = new SelectList(db.Members, "ID", "Name");
+            return View();
+        }
+
+        // POST: Games/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,PlayerOneID,PlayerTwoID,GameDateTime,WinnerID,IsDraw,PlayerOneCurrentRank,PlayerTwoCurrentRank,PlayerOneRankAfterGame,PlayerTwoRankAfterGame")] Game game)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Games.Add(game);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.PlayerOneID = new SelectList(db.Members, "ID", "Name", game.PlayerOneID);
+            ViewBag.PlayerTwoID = new SelectList(db.Members, "ID", "Name", game.PlayerTwoID);
+            ViewBag.WinnerID = new SelectList(db.Members, "ID", "Name", game.WinnerID);
+            return View(game);
+        }
+
+        // GET: Games/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Game game = db.Games.Find(id);
+            if (game == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.PlayerOneID = new SelectList(db.Members, "ID", "Name", game.PlayerOneID);
+            ViewBag.PlayerTwoID = new SelectList(db.Members, "ID", "Name", game.PlayerTwoID);
+            ViewBag.WinnerID = new SelectList(db.Members, "ID", "Name", game.WinnerID);
+            return View(game);
+        }
+
+        // POST: Games/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,PlayerOneID,PlayerTwoID,GameDateTime,WinnerID,IsDraw,PlayerOneCurrentRank,PlayerTwoCurrentRank,PlayerOneRankAfterGame,PlayerTwoRankAfterGame")] Game game)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(game).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.PlayerOneID = new SelectList(db.Members, "ID", "Name", game.PlayerOneID);
+            ViewBag.PlayerTwoID = new SelectList(db.Members, "ID", "Name", game.PlayerTwoID);
+            ViewBag.WinnerID = new SelectList(db.Members, "ID", "Name", game.WinnerID);
+            return View(game);
+        }
+
+        // GET: Games/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Game game = db.Games.Find(id);
+            if (game == null)
+            {
+                return HttpNotFound();
+            }
+            return View(game);
+        }
+
+        // POST: Games/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Game game = db.Games.Find(id);
+            db.Games.Remove(game);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
